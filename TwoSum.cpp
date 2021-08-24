b333d2d709de0dde52e667fb2158e2ebbe524546
Abha Himani <74365827+AbhaHimani@users.noreply.github.com>
2021-08-24 20:33:11 +0530

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -10,20 +10,23 @@ int main()
     cin >> n;
     int arr[n];
     int C[n];
+    //Take the numbers as input for arr, and make a copy of that, array C
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
         C[i] = arr[i];
     }
+    //Sort the array
     sort(arr, arr + n);
     int target;
     cin >> target;
     int i = 0;
+    //these two --> x and y are used to take the value of array element whixh when summed up result to target
     int m, x;
     while (i < n - 1)
     //3,2,4
     //2,3,4
-
+    // here, 1,2,3,4 --> we point to 1 and 4, and sum them, if target is more than the sum, we move ahead in array elements, else move backward from end
     {
         if (arr[i] + arr[n - 1] == target)
         {
@@ -41,6 +44,7 @@ int main()
             n--;
         }
     }
+    // here we will, find the index lol
     for (int i = 0; i < n; i++)
     {
         if (m == C[i])
